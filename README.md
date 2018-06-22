4c1c9a013abda4175393a237e046dc15157efebb
noffle <sww@eight45.net>
2018-06-22 10:17:50 -0700

    feat: add unpack and unpackIfNew APIs

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # mapeo-styles
-Default styles &amp; tiles for mapeo backgrounds
+> Default styles & tiles for mapeo backgrounds
+
+Static resources for Mapeo Mobile & Desktop.
+
+## API
+
+```js
+var styles = require('mapeo-styles')
+```
+
+### styles.unpack(root, cb)
+
+Unpacks the `styles/` and `presets/` directories to the file path `root`, and calls `cb` upon completion or error.
+
+### styles.unpackIfNew(root, cb)
+
+The same as `styles.unpack`, except a `version` file will also be written to track what version of the style/preset data was unpacked. This lets the unpacking step be nearly zero-cost when the version of the data has not been changed.
+
+## License
+
+ISC
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+var fs = require('fs')
+var ncp = require('ncp')
+var path = require('path')
+
 module.exports = {}
+
+module.exports.unpack = function (root, cb) {
+  ncp(path.join(__dirname, 'presets'), path.join(root, 'presets'), function (err) {
+    if (err) return cb(err)
+    ncp(path.join(__dirname, 'styles'), path.join(root, 'styles'), cb)
+  })
+}
+
+module.exports.unpackIfNew = function (root, cb) {
+  var moduleVersion = require(path.join(__dirname, 'package.json')).version
+
+  // Bail immediately if version file exists and is set to the current module version.
+  var versionPath = path.join(root, 'version')
+  if (fs.existsSync(versionPath)) {
+    var fsVersion = fs.readFileSync(versionPath, 'utf8')
+    if (fsVersion === moduleVersion) return process.nextTick(cb, null, false)
+  }
+
+  // Unpack presets & styles
+  module.exports.unpack(root, function (err) {
+    if (err) return cb(err)
+
+    // Write version file
+    fs.writeFile(path.join(root, 'version'), moduleVersion, 'utf8', function (err) {
+      cb(err, true)
+    })
+  })
+}
